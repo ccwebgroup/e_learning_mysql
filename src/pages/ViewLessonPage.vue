9e8ad6f24aabb53138d2784e4dc2ab08c0a8bf05
ccwebgroup <ccwebgroup@gmail.com>
2023-02-17 18:20:53 +0800

    at home devs

--- src/pages/ViewLessonPage.vue
+++ src/pages/ViewLessonPage.vue
@@ -2,10 +2,13 @@
   <q-page padding>
     <div v-if="lesson">
       <transition appear enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
-        <div flat v-show="!loading" class="q-pa-sm">
-          <div v-html="lesson.content"></div>
+        <div flat v-show="!loading" class="q-pa-sm ">
+          <q-card class="lesson-card q-mb-md">
+            <q-card-section class="text-h6">{{ lesson.title }}</q-card-section>
+          </q-card>
+          <div v-html="lesson.content" style="margin-bottom: 90px;"></div>
 
-          <div class="rounded-borders bg-amber-9">
+          <div class="rounded-borders bg-amber-9 bordered absolute-bottom q-mx-md q-mb-sm">
             <q-item-label header class="text-h6 text-white">Test Yourself With Exercises</q-item-label>
             <div class="bg-white q-pa-md">
               <q-btn :to="`/exercises/${lesson.id}`" label="Start the exercises" color="primary" />
@@ -26,11 +29,17 @@
     </q-page-scroller>
   </q-page>
 </template>
-<style>
+<style lang="scss">
 pre {
   background: rgb(22, 22, 22);
   color: white;
   padding: 10px;
+}
+
+
+.lesson-card {
+  border-radius: 5px;
+  border-left: 10px solid $amber-9;
 }
 </style>
 
