5af92ab2c50df3daaea2bcdea4c5843e7395fc12
ccwebgroup <ccwebgroup@gmail.com>
2023-02-17 18:58:52 +0800

    Fixed design

--- src/pages/ViewExercisesPage.vue
+++ src/pages/ViewExercisesPage.vue
@@ -1,49 +1,61 @@
 <template>
-  <q-page padding>
-    <div v-if="exercises.length">
-      <q-card>
-        <q-card-section>
-          <div class="bg-grey-9 rounded-borders q-pa-sm text-white">
-            <div class="text-h6  text-amber-6">
-              Test Yourself!
+  <q-page padding class="flex flex-center">
+    <div>
+      <div v-if="exercises.length">
+        <q-card>
+          <q-card-section>
+            <div class="bg-grey-9 rounded-borders q-pa-sm text-white">
+              <div class="text-h6  text-amber-6">
+                Test Yourself!
+              </div>
+              <q-item-label class="text-subtitle1">Exercise No. {{
+                exercises[current - 1].no
+              }}</q-item-label>
+              <q-item-label class="text-subtitle1">Points: <span class="text-positive">{{
+                exercises[current - 1].points
+              }}</span>
+              </q-item-label>
             </div>
-            <q-item-label class="text-subtitle1">Exercise No. {{
-              exercises[current - 1].no
-            }}</q-item-label>
-            <q-item-label class="text-subtitle1">Points: <span class="text-positive">{{
-              exercises[current - 1].points
-            }}</span>
-            </q-item-label>
-          </div>
-          <div class="q-py-md q-px-sm text-subtitle1">
-            {{ exercises[current - 1].instruction }}
-          </div>
-
-          <!-- Choices Display -->
-          <div class="q-px-md rounded-borders bg-amber-1">
-            <div v-for="item in exercises[current
-              - 1].choices" :key="item.index">
-              <q-radio v-model="answer" :val="item.index" :disable="checkIfTaken(exercises[current - 1])"
-                :label="item.text" checked-icon="task_alt" />
-              <q-chip v-if="checkIfTaken(exercises[current - 1]) && exercises[current - 1].answer == item.index"
-                color="positive" size="sm" text-color="white">Answer</q-chip>
+            <div class="q-py-md q-px-sm text-subtitle1">
+              {{ exercises[current - 1].instruction }}
             </div>
-          </div>
 
-          <!-- <ExerciseForm :exercise="exercises[current - 1]" /> -->
-          <q-card-actions align="right">
-            <q-btn @click="submitAnswer(exercises[current - 1])" :disable="checkIfTaken(exercises[current - 1])"
-              :loading="isLoading" unelevated padding="2px 20px"
-              :label="checkIfTaken(exercises[current - 1]) ? 'Done!' : 'Submit'"
-              :color="checkIfTaken(exercises[current - 1]) ? 'dark' : 'primary'" no-caps />
-          </q-card-actions>
-        </q-card-section>
+            <!-- Choices Display -->
+            <div class="q-px-md rounded-borders bg-amber-1">
+              <div v-for="item in exercises[current
+                - 1].choices" :key="item.index">
+                <q-radio v-model="answer" :val="item.index" :disable="checkIfTaken(exercises[current - 1])"
+                  :label="item.text" checked-icon="task_alt" />
+                <q-chip v-if="checkIfTaken(exercises[current - 1]) && exercises[current - 1].answer == item.index"
+                  color="positive" size="sm" text-color="white">Answer</q-chip>
+              </div>
+            </div>
 
-      </q-card>
-      <div class="flex flex-center q-pa-md">
-        <q-pagination @update:model-value="(val) => answer = ''" v-model="current" :max="exercises.length" :max-pages="6"
-          direction-links outline color="amber-9" active-design="unelevated" active-color="brown"
-          active-text-color="white" />
+            <!-- <ExerciseForm :exercise="exercises[current - 1]" /> -->
+            <q-card-actions align="right" class="q-pt-md">
+              <q-btn @click="submitAnswer(exercises[current - 1])" :disable="checkIfTaken(exercises[current - 1])"
+                :loading="isLoading" unelevated padding="2px 20px"
+                :label="checkIfTaken(exercises[current - 1]) ? 'Done, you can only take this quiz once!' : 'Submit'"
+                :color="checkIfTaken(exercises[current - 1]) ? 'dark' : 'primary'" no-caps />
+            </q-card-actions>
+          </q-card-section>
+
+        </q-card>
+        <div class="flex flex-center q-pa-md">
+          <q-pagination @update:model-value="(val) => answer = ''" v-model="current" :max="exercises.length"
+            :max-pages="6" direction-links outline color="amber-9" active-design="unelevated" active-color="brown"
+            active-text-color="white" />
+        </div>
+      </div>
+      <div v-else>
+
+        <q-banner rounded class="bg-primary text-white" style="max-width: 600px;">
+          No exercises added on this lesson yet. Please come back next time to earn more points.
+
+          <template v-slot:action>
+            <q-btn @click="$router.go(-1)" icon="arrow_back" flat label="G0 Back" />
+          </template>
+        </q-banner>
       </div>
     </div>
 </q-page>
--- src/pages/ViewLessonPage.vue
+++ src/pages/ViewLessonPage.vue
@@ -10,8 +10,11 @@
 
           <div class="rounded-borders bg-amber-9 bordered absolute-bottom q-mx-md q-mb-sm">
             <q-item-label header class="text-h6 text-white">Test Yourself With Exercises</q-item-label>
-            <div class="bg-white q-pa-md">
+            <div v-if="authUser" class="bg-white q-pa-md">
               <q-btn :to="`/exercises/${lesson.id}`" label="Start the exercises" color="primary" />
+            </div>
+            <div v-else class="bg-white q-pa-md">
+              <q-btn to="/auth/login" label="Log in to answer" color="primary" />
             </div>
           </div>
         </div>
@@ -27,7 +30,7 @@
     <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[18, 18]">
       <q-btn fab icon="keyboard_arrow_up" color="primary" />
     </q-page-scroller>
-  </q-page>
+</q-page>
 </template>
 <style lang="scss">
 pre {
@@ -57,10 +60,12 @@ img
 <script setup>
 import { onBeforeMount, computed, ref, watch } from 'vue';
 import { lessonStore } from 'src/stores/lessons';
+import { authStore } from 'src/stores/auth';
 import { useRoute } from 'vue-router';
 
 const loading = ref(true);
 const route = useRoute()
+const authUser = computed(() => authStore().authUser)
 const lesson = computed(() => lessonStore().lesson)
 
 watch(lesson, (newVal, oldVal) => {
